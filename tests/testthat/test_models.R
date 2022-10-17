4d1d2c2fc582805d16faaa3eec2db150aaa917e4
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-10-17 12:28:40 -0700

    🛑🧪 Tests other functions

--- tests/testthat/test_models.R
+++ tests/testthat/test_models.R
@@ -1,7 +1,17 @@
-describe("Get name null-model", {
-  it("Right name", {
+describe("Get name models", {
+  it("Right name: null-model", {
     expected <- "null-model"
     obtained <- null_model(0)
+    expect_equal(obtained, expected)
+  })
+  it("Right name: full-model", {
+    expected <- "full-model"
+    obtained <- full_model(0)
+    expect_equal(obtained, expected)
+  })
+  it("Right name: stepwise-model", {
+    expected <- "stepwise-model"
+    obtained <- stepwise_model(0)
     expect_equal(obtained, expected)
   })
 })
