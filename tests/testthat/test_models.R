98ea7bebee1dee3e0ae07e7f8931b42a24161996
Alejandro Padilla <alejandro.padilla@islas.org.mx>
2022-10-25 08:27:22 -0700

    ⚗️ Add tests for `full_model`

--- tests/testthat/test_models.R
+++ tests/testthat/test_models.R
@@ -1,5 +1,5 @@
 describe("Get name models", {
-  titanic <- read.csv("../data/titanic_data.csv")
+  titanic <- read.csv("tests/data/titanic_data.csv")
   it("Right name: null-model", {
     titanic_null <- null_model(titanic)
     obtained <- titanic_null$coefficients
@@ -8,9 +8,11 @@ describe("Get name models", {
     expect_equal(obtained, expected, tolerance = 1e-3)
   })
   it("Right name: full-model", {
-    expected <- "full-model"
-    obtained <- full_model(0)
-    expect_equal(obtained, expected)
+    titanic_full <- full_model(titanic)
+    expected <- c(-0.1230, -0.2785, 0.1238, 0.3184)
+    obtained <- titanic_full$coefficients
+    names(obtained) <- c()
+    expect_equal(obtained, expected, tolerance = 1e-3)
   })
   it("Right name: stepwise-model", {
     expected <- "stepwise-model"
